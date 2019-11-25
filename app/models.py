03bede96c66a3d2f908e23cce0b75ca563d7ed5d
vique254 <victoronyango051@gmail.com>
2019-11-25 12:55:06 +0300

    create a class user

--- app/models.py
+++ app/models.py
@@ -1,40 +1,10 @@
-class Pitch:
-    '''
-    Pitch class to define Pitch Objects
-    '''
+from . import db
 
-    def __init__(self,id,title,overview,vote_count):
-        self.id =id
-        self.title = title
-        self.vote_count = vote_count
-
-
-
-class Review:
-
-    all_reviews = []
-
-    def __init__(self,pitch_id,title,review):
-        self.movie_id = movie_id
-        self.title = title
-        self.review = review
-
-
-    def save_review(self):
-        Review.all_reviews.append(self)
-
-
-    @classmethod
-    def clear_reviews(cls):
-        Review.all_reviews.clear()
-
-    @classmethod
-    def get_reviews(cls,id):
-
-        response = []
-
-        for review in cls.all_reviews:
-            if review.movie_id == id:
-                response.append(review)
-
-        return response
+class User(db.Model)
+     __tablename__ = 'users'
+     id = db.Column(db.Integer,primary_key = True)
+     username = db.Column(db.String(255))
+     
+     def __repr__(self):
+         return f'User {self.username}'
+     
