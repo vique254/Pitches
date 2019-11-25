026b7e37537de7acae21c10226399a631bb63f6c
vique254 <victoronyango051@gmail.com>
2019-11-25 15:40:33 +0300

    rearrange the indentation positioning

--- app/models.py
+++ app/models.py
@@ -10,6 +10,14 @@ class Comment(db.Model):
     def __repr__(self):
         return 'User {self.name}'
     
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255))
+    comment_id = db.Column(db.Integer,db.ForeignKey('comments.id'))
+    pass_secure = db.Column(db.String(255))
+    
     @property
     def password(self):
         raise AttributeError('You cannot read the password attribute')
@@ -21,13 +29,6 @@ class Comment(db.Model):
     def verify_password(self,password):
         return check_password_hash(self.pass_secure,password)
     
-class User(db.Model):
-    __tablename__ = 'users'
-
-    id = db.Column(db.Integer,primary_key = True)
-    username = db.Column(db.String(255))
-    comment_id = db.Column(db.Integer,db.ForeignKey('comments.id'))
-    pass_secure = db.Column(db.String(255))
     def __repr__(self):
         return 'User {self.username}'
     
