05a7dd7db98588c28a5ccf729d5f3b66171b1001
vique254 <victoronyango051@gmail.com>
2019-11-25 15:36:40 +0300

    make an importation for 2 functions to be used in User model

--- app/models.py
+++ app/models.py
@@ -1,4 +1,6 @@
 from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
+
 
 class Comment(db.Model):
     __tablename__ = 'comments' 
@@ -8,13 +10,24 @@ class Comment(db.Model):
     def __repr__(self):
         return 'User {self.name}'
     
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+    
+    @password.setter
+    def password(self,password):
+        self.pass_secure = generate_password_hash(password)
+    
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure,password)
+    
 class User(db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
     comment_id = db.Column(db.Integer,db.ForeignKey('comments.id'))
-    
+    pass_secure = db.Column(db.String(255))
     def __repr__(self):
         return 'User {self.username}'
     
