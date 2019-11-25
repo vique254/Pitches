f3b1c4bdeaf92fd3cc4f281c2c13ff9ee1f13f45
vique254 <victoronyango051@gmail.com>
2019-11-25 17:23:15 +0300

    create authenticated route logout

--- app/auth/views.py
+++ app/auth/views.py
@@ -3,6 +3,8 @@ from . import auth
 from ..models import User
 from .forms import LoginForm, RegistrationForm
 from .. import db
+from flask_login import login_user,logout_user,login_required
+
 
 @auth.route('/login',methods=['GET','POST'])
 def login():
@@ -28,3 +30,9 @@ def register():
         return redirect(url_for('auth.login'))
         title = "New Account"
     return render_template('auth/register.html',registration_form = form)
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("main.index"))
