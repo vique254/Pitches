837bbbe48bb29ddfa90214efec945ecf0e8e92fc
vique254 <victoronyango051@gmail.com>
2019-11-25 16:25:18 +0300

    import UserMixin,login_manager and add a login decorator

--- app/models.py
+++ app/models.py
@@ -1,7 +1,11 @@
 from . import db
 from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
+from . import login_manager
 
-
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
 class Comment(db.Model):
     __tablename__ = 'comments' 
     id = db.Column(db.Integer,primary_key = True)
@@ -10,11 +14,12 @@ class Comment(db.Model):
     def __repr__(self):
         return 'User {self.name}'
     
-class User(db.Model):
+class User(UserMixin, db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
+    email = db.Column(db.String(255),unique = True,index = True)
     comment_id = db.Column(db.Integer,db.ForeignKey('comments.id'))
     pass_secure = db.Column(db.String(255))
     
