af7db10dd88f1d0160f29d121b04afd1794079da
vique254 <victoronyango051@gmail.com>
2019-11-25 14:56:43 +0300

    import migrate class and MigrateCommand class

--- app/models.py
+++ app/models.py
@@ -3,10 +3,10 @@ from . import db
 class Comment(db.Model):
     __tablename__ = 'comments' 
     id = db.Column(db.Integer,primary_key = True)
-   name = db.Column(db.String(255))
-   users = db.relationship('User',backref ='comment',lazy="dynamic") 
+    name = db.Column(db.String(255))
+    users = db.relationship('User',backref ='comment',lazy="dynamic") 
     def __repr__(self):
-        return f'User {self.name}'
+        return 'User {self.name}'
     
 class User(db.Model):
     __tablename__ = 'users'
@@ -16,5 +16,5 @@ class User(db.Model):
     comment_id = db.Column(db.Integer,db.ForeignKey('comments.id'))
     
     def __repr__(self):
-        return f'User {self.username}'
+        return 'User {self.username}'
     
