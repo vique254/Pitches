e07796251076e575d49a02d6a38d2ce0912243e4
vique254 <victoronyango051@gmail.com>
2019-11-25 16:47:44 +0300

    import ValidationError

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -2,6 +2,8 @@ from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField,SubmitField
 from wtforms.validators import Required,Email,EqualTo
 from ..models import User
+from wtforms import ValidationError
+
 
 class RegistrationForm(FlaskForm):
     email = StringField('Your Email Address',validators=[Required(),Email()])
