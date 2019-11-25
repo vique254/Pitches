5e2f8059422dfe8575dcc239a4105c3b755c7fba
vique254 <victoronyango051@gmail.com>
2019-11-25 14:41:01 +0300

    create class user and comment

--- app/models.py
+++ app/models.py
@@ -1,9 +1,20 @@
 from . import db
 
+class Comment(db.Model):
+    __tablename__ = 'comments' 
+    id = db.Column(db.Integer,primary_key = True)
+   name = db.Column(db.String(255))
+   users = db.relationship('User',backref ='comment',lazy="dynamic") 
+    def __repr__(self):
+        return f'User {self.name}'
+    
 class User(db.Model):
     __tablename__ = 'users'
+
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
+    comment_id = db.Column(db.Integer,db.ForeignKey('comments.id'))
     
     def __repr__(self):
         return f'User {self.username}'
+    
