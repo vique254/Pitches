d798701f77a232937787f1914541d794e91286e7
vique254 <victoronyango051@gmail.com>
2019-11-26 10:22:41 +0300

    make change in structure and add templates

--- config.py
+++ config.py
@@ -1,16 +1,51 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:vique@localhost/pitch'
+
+    '''
+    General configuration parent class
+    '''
+    SECRET_KEY =os.environ.get('SECRET_KEY')
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+    # simple mde  configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+
+
 class ProdConfig(Config):
+    '''
+    Pruduction configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+class TestConfig(Config):
+    '''
+    Testing configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings 
+    '''
     pass
 
-
 class DevConfig(Config):
+    '''
+    Development configuration child class
+    
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://moringa:vique@localhost/pitches'
     DEBUG = True
 
+
 config_options = {
-'development':DevConfig,
-'production':ProdConfig
+    'development': DevConfig,
+    'production': ProdConfig,
+    'test': TestConfig
 }
--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
-export SECRET_KEY='Vique254'
-export MAIL_USERNAME='Victoronyango051@gmail.com'
-export MAIL_PASSWORD='VickyRowz' 
+ export SECRET_KEY='Vique254'
+ export MAIL_USERNAME='Spartanb@gmail.com'
+ export MAIL_PASSWORD='123255KE'
 
 python3.6 manage.py server
